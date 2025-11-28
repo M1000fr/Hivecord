5f9a91f3d08c208b8e231ef10d3053e102be6cb6
M1000fr <formentel.remi@gmail.com>
2025-11-28 13:14:02 +0100

    refactor: remove unused ConfigurationConfig import from ConfigurationModule

--- src/modules/Configuration/ConfigurationModule.ts
+++ src/modules/Configuration/ConfigurationModule.ts
@@ -1,5 +1,4 @@
 import { Module } from "../../decorators/Module";
-import { ConfigurationConfig } from "./ConfigurationConfig";
 import ModulesCommand from "./commands/modules/index";
 import ModuleConfigInteractionHandler from "./events/interactionCreate/moduleConfigHandler";
 import AutocompleteHandlerEvent from "./events/interactionCreate/autocompleteHandler";
@@ -8,6 +7,5 @@ import AutocompleteHandlerEvent from "./events/interactionCreate/autocompleteHan
 	name: "Configuration",
 	commands: [ModulesCommand],
 	events: [ModuleConfigInteractionHandler, AutocompleteHandlerEvent],
-	config: ConfigurationConfig,
 })
 export class ConfigurationModule {}
