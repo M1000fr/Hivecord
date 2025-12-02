4f9688d293192d72f7a33845c36838c7748b1c11
M1000fr <formentel.remi@gmail.com>
2025-12-02 14:23:02 +0100

    Refactor(General): Move interactions to dedicated class

---
+++ src/modules/General/interactions/PingCommandInteractions.ts
@@ -0,0 +1,9 @@
+import { ButtonInteraction } from "discord.js";
+import { Button } from "@decorators/Interaction";
+
+export class PingCommandInteractions {
+    @Button("ping_button")
+    async handlePingButton(interaction: ButtonInteraction) {
+        await interaction.reply({ content: "Pong from interaction!", flags: 64 });
+    }
+}
