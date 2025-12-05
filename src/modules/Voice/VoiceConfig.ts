bbd4f37e7561969ec2cde202ca3adf49c81decae
M1000fr <formentel.remi@gmail.com>
2025-12-05 03:18:54 +0100

    refactor(voice): add localizations for Temp Voice Generator display name and description

--- src/modules/Voice/VoiceConfig.ts
+++ src/modules/Voice/VoiceConfig.ts
@@ -7,7 +7,15 @@ import {
 export class VoiceConfig {
 	@ConfigProperty({
 		displayName: "Temp Voice Generator",
+		displayNameLocalizations: {
+			fr: "Générateur de voix temporaire",
+			"en-US": "Temp Voice Generator",
+		},
 		description: "The channel ID for the temporary voice channel generator",
+		descriptionLocalizations: {
+			fr: "L'ID du canal pour le générateur de canal vocal temporaire",
+			"en-US": "The channel ID for the temporary voice channel generator",
+		},
 		type: EConfigType.Channel,
 	})
 	voiceTempVoiceGeneratorChannelId: string = "";
