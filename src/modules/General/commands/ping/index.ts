235448165e036d451124c437669349f7dbba2ed2
M1000fr <formentel.remi@gmail.com>
2025-12-19 19:30:46 +0100

    refactor(PingCommand): remove MeasureTime decorator and related imports

--- src/modules/General/commands/ping/index.ts
+++ src/modules/General/commands/ping/index.ts
@@ -2,8 +2,6 @@ import { BaseCommand } from "@class/BaseCommand";
 import { Command } from "@decorators/Command";
 import { DefaultCommand } from "@decorators/DefaultCommand";
 import { EPermission } from "@enums/EPermission";
-import { ConfigService } from "@services/ConfigService";
-import { MeasureTime } from "@src/decorators/MeasureTime";
 import { ChatInputCommandInteraction, Client } from "discord.js";
 import type { TFunction } from "i18next";
 import { pingOptions } from "./pingOptions";
@@ -11,11 +9,6 @@ import { pingOptions } from "./pingOptions";
 @Command(pingOptions)
 export default class PingCommand extends BaseCommand {
 	@DefaultCommand(EPermission.Ping)
-	@MeasureTime({
-		name: "ping_command_execution_time",
-		trackInteraction: true,
-		trackComponents: [ConfigService],
-	})
 	async run(
 		client: Client,
 		interaction: ChatInputCommandInteraction,
