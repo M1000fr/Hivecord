7e2d8480adc853bc424cb508371a795a79140e6b
M1000fr <formentel.remi@gmail.com>
2025-12-21 17:00:39 +0100

    BREAKING CHANGE: Remove all uncessery modules to refactory base structure

--- src/modules/Debug/DebugModule.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@decorators/Module";
-import DebugCommand from "./commands/debug/index";
-
-@Module({
-	name: "Debug",
-	commands: [DebugCommand],
-	events: [],
-})
-export class DebugModule {}
--- src/modules/Moderation/commands/purge/purgeOptions.ts
+++
@@ -1,8 +0,0 @@
-import type { CommandOptions } from "@interfaces/CommandOptions";
-import { InteractionContextType } from "discord.js";
-
-export const purgeOptions: CommandOptions = {
-	name: "purge",
-	description: "Nuke and recreate the current channel",
-	contexts: [InteractionContextType.Guild],
-};
