86e3fcca013d28d02ff14c7b533a26b55406e0c0
M1000fr <formentel.remi@gmail.com>
2025-12-22 10:37:11 +0100

    refactor(events): use PagerService in pagerHandler event
    Replace static Pager.handleInteraction() call with injected PagerService:
    - Add constructor with @Inject(PagerService) decorator
    - Call this.pagerService.handleInteraction() instead of static method
    - Follows proper dependency injection pattern

--- src/modules/General/events/interactionCreate/pagerHandler.ts
+++ src/modules/General/events/interactionCreate/pagerHandler.ts
@@ -1,15 +1,18 @@
 import { LeBotClient } from "@class/LeBotClient";
-import { Pager } from "@class/Pager";
 import { EventController } from "@decorators/EventController";
+import { Inject } from "@decorators/Inject";
 import { On } from "@decorators/On";
 import { Client } from "@decorators/params/Client";
 import { Context } from "@decorators/params/Context";
 import { BotEvents } from "@enums/BotEvents";
+import { PagerService } from "@modules/Core/services/PagerService";
 import type { ContextOf } from "@src/types/ContextOf.ts";
 import { DiscordAPIError } from "discord.js";
 
 @EventController()
 export default class PagerHandlerEvent {
+	constructor(@Inject(PagerService) private pagerService: PagerService) {}
+
 	@On(BotEvents.InteractionCreate)
 	async run(
 		@Client() client: LeBotClient<true>,
@@ -21,7 +24,7 @@ export default class PagerHandlerEvent {
 		// Try to handle as pager interaction
 		// If it returns true, it was handled
 		try {
-			await Pager.handleInteraction(interaction);
+			await this.pagerService.handleInteraction(interaction);
 		} catch (error: unknown) {
 			// Ignore unknown interaction errors, as they might happen if the interaction was already handled
 			if (
