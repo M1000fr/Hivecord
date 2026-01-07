43e949d0f096a050ee148b7786e63691453f5b6d
M1000fr <formentel.remi@gmail.com>
2026-01-07 16:39:58 +0100

    refactor: remove custom config type

--- src/modules/Configuration/ConfigurationModule.ts
+++ src/modules/Configuration/ConfigurationModule.ts
@@ -2,8 +2,6 @@ import { Global } from "@decorators/Global";
 import { Module } from "@decorators/Module";
 import { DatabaseModule } from "@modules/Database/DatabaseModule";
 import ModulesCommand from "./commands/modules/index";
-import { BooleanConfigHandler } from "./configHandler/BooleanConfigHandler";
-import { StringConfigHandler } from "./configHandler/StringConfigHandler";
 import { AttachmentConfigInteractions } from "./interactions/config/AttachmentConfigInteractions";
 import { BooleanConfigInteractions } from "./interactions/config/BooleanConfigInteractions";
 import { ModuleConfigInteractions } from "./interactions/config/ModuleConfigInteractions";
@@ -18,28 +16,25 @@ import { RoleConfigService } from "./services/RoleConfigService";
 
 @Global()
 @Module({
-	name: "Configuration",
-	imports: [DatabaseModule],
-	providers: [
-		// Services
-		ConfigCacheService,
-		ChannelConfigService,
-		RoleConfigService,
-		ConfigService,
-		// Commands
-		ModulesCommand,
-		// Interactions
-		ModuleConfigInteractions,
-		BooleanConfigInteractions,
-		StringConfigInteractions,
-		StringChoiceConfigInteractions,
-		RoleChannelConfigInteractions,
-		AttachmentConfigInteractions,
-		StringArrayConfigInteractions,
-		// Config Handlers
-		BooleanConfigHandler,
-		StringConfigHandler,
-	],
-	exports: [ConfigService],
+  name: "Configuration",
+  imports: [DatabaseModule],
+  providers: [
+    // Services
+    ConfigCacheService,
+    ChannelConfigService,
+    RoleConfigService,
+    ConfigService,
+    // Commands
+    ModulesCommand,
+    // Interactions
+    ModuleConfigInteractions,
+    BooleanConfigInteractions,
+    StringConfigInteractions,
+    StringChoiceConfigInteractions,
+    RoleChannelConfigInteractions,
+    AttachmentConfigInteractions,
+    StringArrayConfigInteractions,
+  ],
+  exports: [ConfigService],
 })
 export class ConfigurationModule {}
