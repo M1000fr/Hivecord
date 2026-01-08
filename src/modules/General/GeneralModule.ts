a7bb72bbf8ab9baa98d2bb807a568732a09d892e
M1000fr <formentel.remi@gmail.com>
2026-01-08 15:20:55 +0100

    refactor(general): remove redundant ConfigurationModule import

--- src/modules/General/GeneralModule.ts
+++ src/modules/General/GeneralModule.ts
@@ -1,18 +1,11 @@
 import { Module } from "@decorators/Module";
-import { ConfigurationModule } from "@modules/Configuration/ConfigurationModule";
 import { PingCommand } from "./commands/ping";
 import ReadyEvent from "./events/ready/log";
 import { GeneralConfig } from "./GeneralConfig";
 
 @Module({
 	name: "General",
-	imports: [ConfigurationModule],
 	config: GeneralConfig,
-	providers: [
-		// Events
-		ReadyEvent,
-		PingCommand,
-	],
-	exports: [],
+	providers: [ReadyEvent, PingCommand],
 })
 export class GeneralModule {}
